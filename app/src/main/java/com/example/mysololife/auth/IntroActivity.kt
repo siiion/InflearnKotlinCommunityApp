29ab68d1a301adb679f1a5decaa49033457eca2c
Siwon Jeon <siwonjeon234@gmail.com>
2023-05-09 17:16:14 +0900

    Section 2

--- app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
@@ -3,11 +3,18 @@ package com.example.mysololife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
 import com.example.mysololife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var binding: ActivityIntroBinding
 
@@ -15,6 +22,8 @@ class IntroActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        auth = Firebase.auth
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
@@ -26,6 +35,21 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
